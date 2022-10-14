2ada4da35a3c588eda9bb944c247c770922562b1
David <jdavid690@outlook.es>
2022-10-14 07:42:34 -0500

    icon

--- template/encabezado.php
+++ template/encabezado.php
@@ -13,6 +13,9 @@ if(!isset($_SESSION["username"])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <!-- link icon pestaña -->
+    <link rel="icon" href="./img/favicon.png"></link>
+
     <!-- link estilos  -->
     <link rel="stylesheet" href="styles/style.css">
 
@@ -24,8 +27,12 @@ if(!isset($_SESSION["username"])) {
 
     <!-- script de switAlert -->
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    
+    
+    <!-- fontwamesomw -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-    <title>Aplika Control Corrosion</title>
+    <title> Aplika <?php echo $_GET['pag_actual'] ?></title>
 </head>
 <body>
 
@@ -46,13 +53,13 @@ $Pagina = $_GET['pag_actual'];
 
 <header>
         <nav class="menu">
-            <img src="img/logoAPK.png" alt="">
+            <img src="img/logoAPK.png" href="home.php?pag_actual=Inicio">
             <ul><!---------------------------------->
-                <li><a class="underline" <?php if($Pagina == $Inicio) { echo ("style='color: orange;'"); } ?> href="home.php?pag_actual=Inicio">Inicio</a></li>
-                <li><a class="underline" <?php if($Pagina == $Productos) { echo ("style='color: orange;'"); } ?> href="producto.php?pag_actual=Productos">Productos</a></li>
-                <li><a class="underline" <?php if($Pagina == $Clientes) { echo ("style='color: orange;'"); } ?> href="client.php?pag_actual=Clientes">Clientes</a></li>
-                <li><a class="underline" <?php if($Pagina == $Solicitud) { echo ("style='color: orange;'"); } ?> href="Datos_proyecto.php?pag_actual=Solicitud">Solicitud</a></li>
-                <li><a class="underline" href="cerrar_sesion.php">Salir</a></li>
+                <li><a class="underline" <?php if($Pagina == $Inicio) { echo ("style='color: #F28E2A;'"); } ?> href="home.php?pag_actual=Inicio"><i class="fa-solid fa-house"></i>&nbspInicio</a></li>
+                <li><a class="underline" <?php if($Pagina == $Productos) { echo ("style='color: #F28E2A;'"); } ?> href="producto.php?pag_actual=Productos"><i class="fa-brands fa-product-hunt"></i>&nbspProductos</a></li>
+                <li><a class="underline" <?php if($Pagina == $Clientes) { echo ("style='color: #F28E2A;'"); } ?> href="clientes.php?pag_actual=Clientes"><i class="fa-solid fa-users"></i>&nbspClientes</a></li>
+                <li><a class="underline" <?php if($Pagina == $Solicitud) { echo ("style='color: #F28E2A;'"); } ?> href="Datos_proyecto.php?pag_actual=Solicitud">Solicitud <i class="fa-light fa-list-dropdown"></i></a></li>
+                <li><a class="underline" href="cerrar_sesion.php">Salir &nbsp<i class="fa-solid fa-right-from-bracket"></i></a></li>
             </ul>
         </nav>
 </header>

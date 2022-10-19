3a9b1333b06a3ca9e4f21f63cad45b9090104748
Deiby Prada <deibyfabianpradaquintero@gmail.com>
2022-10-19 16:47:12 -0500

    Cambios en select, combinacion con marca y solidos

--- producto/producto.js
+++ producto/producto.js
@@ -14,7 +14,21 @@ function obtener_imprimante(marca,solidos)
     })
 }
 
-$(document).on('change', '#select_solidos','#select_marca', function()
+$(document).on('change', '#select_solidos', function()
+{
+    var marca = $("#select_marca").val();
+    var solidos = $("#select_solidos").val();
+    if (marca != "0" && solidos != "0")
+    {
+        obtener_imprimante(marca,solidos);
+    }
+    else
+    {
+        obtener_imprimante();
+    }
+});
+
+$(document).on('change','#select_marca', function()
 {
     var marca = $("#select_marca").val();
     var solidos = $("#select_solidos").val();

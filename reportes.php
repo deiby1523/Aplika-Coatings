ce59a10f11e45d312f71b65be1ab8bf2e925178a
Deiby Prada <deibyfabianpradaquintero@gmail.com>
2022-10-27 08:01:02 -0500

    Generar PDF

---
+++ reportes.php
@@ -0,0 +1,109 @@
+<?php 
+
+require_once 'librerias/dompdf/autoload.inc.php';
+use Dompdf\Dompdf;
+$dompdf = new Dompdf();
+
+ob_start(); //iniciamos un output buffer
+
+?>
+
+
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+<?php
+#var_dump($_POST);
+$cod_imprimante = $_POST['cod_imprimante'];
+$cod_barrera = $_POST['cod_barrera'];
+$cod_acabado = $_POST['cod_acabado'];
+
+$imprimante_espesor = $_POST['imprimante_espesor'];
+$imprimante_rendimiento_teorico = $_POST['imprimante_rendimiento_teorico'];
+$imprimante_desperdicio = $_POST['imprimante_desperdicio'];
+$imprimante_rendimiento_practico = $_POST['imprimante_rendimiento_practico'];
+$imprimante_area = $_POST['imprimante_area'];
+$imprimante_galones = $_POST['imprimante_galones'];
+$imprimante_unidades = $_POST['imprimante_unidades'];
+
+
+$db = 'recubrimientos';
+$host = 'localhost';
+$username = 'root';
+$password = '123456';
+$dbname = 'recubrimientos';
+
+$conexion = mysqli_connect( $host, $username, $password ) or die ("No se ha podido conectar al servidor de Base de datos");
+$db = mysqli_select_db( $conexion, $dbname) or die ("No se ha podido conectar a la base de datos");
+
+
+# Consulta de productos desde la base de datos ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# Codigos de productos ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+$consulta = "SELECT * FROM productos WHERE referencia = '$cod_imprimante'";
+$resultado = mysqli_query( $conexion, $consulta ) or die ( "Algo ha ido mal en la consulta a la base de datos");
+$imprimante = mysqli_fetch_array( $resultado );
+
+
+$consulta = "SELECT * FROM productos WHERE referencia = '$cod_barrera'";
+$resultado = mysqli_query( $conexion, $consulta ) or die ( "Algo ha ido mal en la consulta a la base de datos");
+$barrera = mysqli_fetch_array( $resultado );
+
+$consulta = "SELECT * FROM productos WHERE referencia = '$cod_acabado'";
+$resultado = mysqli_query( $conexion, $consulta ) or die ( "Algo ha ido mal en la consulta a la base de datos");
+$acabado = mysqli_fetch_array( $resultado );
+
+?>
+<br><br><br>
+<div class="container">
+<table class='table' style='width: 100%; margin: 0%'>
+
+	<thead>
+		<tr>
+			<th style='text-align: center; vertical-align: middle;' class='col-sm-1'>Código</th>
+			<th style='text-align: center; vertical-align: middle;' class='col-md-3'>Producto</th>
+			<th style='text-align: center; vertical-align: middle;' class='col-sm-1'>Presentacion</th>
+			<th style='text-align: center; vertical-align: middle;' class='col-sm-1'>Solidos % </th>
+			<th style='text-align: center; vertical-align: middle;' class='col-sm-1'>EPS(Mils)</th>
+			<th style='text-align: center; vertical-align: middle;'>Rendimiento Teórico Mts2/GAL</th>
+            <th style='text-align: center; vertical-align: middle;'>Desperdicio</th>
+			<th style='text-align: center; vertical-align: middle;'>Rendimiento Práctico Mt2/GAL</th>
+            <th style='text-align: center; vertical-align: middle;' class='col-sm-2'>Area</th>
+            <th style='text-align: center; vertical-align: middle;'>Cantidad de galones</th>
+            <th style='text-align: center; vertical-align: middle;'>Cantidad de Unidades</th>
+		</tr>
+	  </thead>
+	  <tbody>
+        <tr>
+        <?php 
+            echo "<td>" . $imprimante['referencia'] . "</td><td>" . $imprimante['producto'] . "</td><td>" . $imprimante['presentacion'] . "</td><td>" . $imprimante['solidos'] . "</td><td>" . $_POST['imprimante_espesor'] . "</td><td>" . $_POST['imprimante_rendimiento_teorico'] . "</td><td>" . $_POST['imprimante_desperdicio'] . "</td><td>" . $_POST['imprimante_rendimiento_practico'] . "</td><td>" . $_POST['imprimante_area'] . "</td><td>" . round($_POST['imprimante_galones'], 2) . "</td><td>" . $_POST['imprimante_unidades'] . "</td>";
+        ?>
+        </tr>   
+</div>
+<?php
+
+require_once('reportes.php'); // llamamos el archivo que se supone contiene el html y dejamoso que se renderize
+$dompdf->load_html(ob_get_clean());//y ponemos todo lo que se capturo con ob_start() para que sea capturado por DOMPDF
+
+$dompdf->set_paper('A4', 'landscape');
+$dompdf->render();
+$dompdf->stream('document.pdf');
+
+/*
+
+require_once 'librerias/dompdf/autoload.inc.php';
+use Dompdf\Dompdf;
+$dompdf = new Dompdf();
+
+ob_start(); //iniciamos un output buffer
+
+require_once('reportes.php'); // llamamos el archivo que se supone contiene el html y dejamoso que se renderize
+$dompdf->load_html(ob_get_clean());//y ponemos todo lo que se capturo con ob_start() para que sea capturado por DOMPDF
+
+$dompdf->set_paper('A4', 'landscape');
+$dompdf->render();
+$dompdf->stream('document.pdf');
+
+*/
+
+
+?>
--- tabla_general.php
+++ tabla_general.php
@@ -1,4 +1,11 @@
-<?php include("template/encabezado.php"); ?>
+<?php 
+
+#require_once 'librerias/dompdf/autoload.inc.php';
+#use Dompdf\Dompdf;
+#$dompdf = new Dompdf();
+#ob_start(); //iniciamos un output buffer
+
+include("template/encabezado.php"); ?>
 
 
 <?php
@@ -46,11 +53,30 @@ $acabado = mysqli_fetch_array( $resultado );
 
 
 
-var_dump( $_POST );
+#var_dump( $_POST );
 
 ?>
-
+<form method="POST" action="reportes.php">
 <div class="container">
+    <input type="hidden" name="cod_imprimante" value="<?php echo $cod_imprimante; ?>">
+    <input type="hidden" name="cod_barrera" value="<?php echo $cod_barrera; ?>">
+    <input type="hidden" name="cod_acabado" value="<?php echo $cod_acabado; ?>">    
+<div class="row button_cancelarCliente">
+            <div class="col-md-2">
+                <div class="d-grid gap-2">
+		    		<a class="btn btn-danger" href="home.php">Cancelar</a>
+            	</div>
+		    </div>
+            <div class="col-md-3">
+		    	<div class="d-grid gap-2" style="float: end;">
+		    		<button class="btn btn-succes" type="submit">Generar PDF</button>
+            	</div>
+		    </div>
+        </div>
+</div>
+<br>
+<div class="container">
+  
     <table class='table table-hover table-striped-columns' style='width: 100%; margin: 0%'>
 	<thead>
 		<tr>
@@ -66,8 +92,11 @@ var_dump( $_POST );
             <th style='text-align: center; vertical-align: middle;'>Cantidad de galones</th>
             <th style='text-align: center; vertical-align: middle;'>Cantidad de Unidades</th>
 		</tr>
-	</thead>
-	<tbody>
+	  </thead>
+	  <tbody>
+
+
+    
     <?php
     #--------------------------------------------------------------------------------------- Imprimación --------------------------------------------------------------------------------------------------------
         if ($cod_imprimante != 0) {
@@ -77,19 +106,19 @@ var_dump( $_POST );
             echo "<td id='imprimante_presentacion'>" . $imprimante['presentacion'] . "</td>";
             echo "<td id='imprimante_solidos'>" . $imprimante['solidos'] . "</td>";
 
-            echo "<td><input id='imprimante_espesor' type='number' class='form-control' value='" . $imprimante_espesor. "'></td>";
+            echo "<td><input id='imprimante_espesor' name='imprimante_espesor' type='number' class='form-control' value='" . $imprimante_espesor. "'></td>";
       
-            echo "<td><input disabled class='form-control' id='imprimante_rendimiento_teorico'></td>";
+            echo "<td><input readonly class='form-control' name='imprimante_rendimiento_teorico' id='imprimante_rendimiento_teorico'></td>";
 
-            echo "<td><input id='imprimante_desperdicio' type='number' class='form-control' value='" . $imprimante_desperdicio. "'></td>";
+            echo "<td><input id='imprimante_desperdicio' type='number' name='imprimante_desperdicio' class='form-control' value='" . $imprimante_desperdicio. "'></td>";
 
-            echo "<td><input disabled class='form-control' id='imprimante_rendimiento_practico'></td>";
+            echo "<td><input readonly class='form-control' name='imprimante_rendimiento_practico' id='imprimante_rendimiento_practico'></td>";
 
-            echo "<td><input id='imprimante_area' type='number' class='form-control' value='" . $imprimante_area. "'></td>";
+            echo "<td><input id='imprimante_area' name='imprimante_area' type='number' class='form-control' value='" . $imprimante_area. "'></td>";
 
-            echo "<td><input disabled class='form-control' id='imprimante_galones'></td>";
+            echo "<td><input readonly class='form-control' name='imprimante_galones' id='imprimante_galones'></td>";
 
-            echo "<td><input disabled class='form-control' id='imprimante_unidades'></td>";
+            echo "<td><input readonly class='form-control' name='imprimante_unidades' id='imprimante_unidades'></td>";
 
             echo "</tr>";
         }
@@ -103,19 +132,19 @@ var_dump( $_POST );
             echo "<td id='barrera_presentacion'>" . $barrera['presentacion'] . "</td>";
             echo "<td id='barrera_solidos'>" . $barrera['solidos'] . "</td>";
 
-            echo "<td><input id='barrera_espesor' type='number' class='form-control' value='" . $barrera_espesor. "'></td>";
+            echo "<td><input id='barrera_espesor' name='barrera_espesor' type='number' class='form-control' value='" . $barrera_espesor. "'></td>";
       
-            echo "<td><input disabled class='form-control' id='barrera_rendimiento_teorico'></td>";
+            echo "<td><input readonly class='form-control' name='barrera_rendimiento_teorico' id='barrera_rendimiento_teorico'></td>";
 
-            echo "<td><input id='barrera_desperdicio' type='number' class='form-control' value='" . $barrera_desperdicio. "'></td>";
+            echo "<td><input id='barrera_desperdicio' name='barrera_desperdicio' type='number' class='form-control' value='" . $barrera_desperdicio. "'></td>";
 
-            echo "<td><input disabled class='form-control' id='barrera_rendimiento_practico'></td>";
+            echo "<td><input readonly class='form-control' name='barrera_rendimiento_practico' id='barrera_rendimiento_practico'></td>";
 
-            echo "<td><input id='barrera_area' type='number' class='form-control' value='" . $barrera_area. "'></td>";
+            echo "<td><input id='barrera_area' type='number' name='barrera_area' class='form-control' value='" . $barrera_area. "'></td>";
 
-            echo "<td><input disabled class='form-control' id='barrera_galones'></td>";
+            echo "<td><input readonly class='form-control' name='barrera_galones' id='barrera_galones'></td>";
 
-            echo "<td><input disabled class='form-control' id='barrera_unidades'></td>";
+            echo "<td><input readonly class='form-control' name='barrera_unidades' id='barrera_unidades'></td>";
 
             echo "</tr>";
         }
@@ -128,19 +157,19 @@ var_dump( $_POST );
             echo "<td id='acabado_presentacion'>" . $acabado['presentacion'] . "</td>";
             echo "<td id='acabado_solidos'>" . $acabado['solidos'] . "</td>";
 
-            echo "<td><input id='acabado_espesor' type='number' class='form-control' value='" . $acabado_espesor. "'></td>";
+            echo "<td><input id='acabado_espesor' name='acabado_espesor' type='number' class='form-control' value='" . $acabado_espesor. "'></td>";
       
-            echo "<td><input disabled class='form-control' id='acabado_rendimiento_teorico'></td>";
+            echo "<td><input readonly class='form-control' name='acabado_rendimiento_teorico' id='acabado_rendimiento_teorico'></td>";
 
-            echo "<td><input id='acabado_desperdicio' type='number' class='form-control' value='" . $acabado_desperdicio. "'></td>";
+            echo "<td><input id='acabado_desperdicio' name='acabado_desperdicio' type='number' class='form-control' value='" . $acabado_desperdicio. "'></td>";
 
-            echo "<td><input disabled class='form-control' id='acabado_rendimiento_practico'></td>";
+            echo "<td><input readonly class='form-control' name='acabado_rendimiento_practico' id='acabado_rendimiento_practico'></td>";
 
-            echo "<td><input id='acabado_area' type='number' class='form-control' value='" . $acabado_area. "'></td>";
+            echo "<td><input id='acabado_area' type='number' name='acabado_area' class='form-control' value='" . $acabado_area. "'></td>";
 
-            echo "<td><input disabled class='form-control' id='acabado_galones'></td>";
+            echo "<td><input readonly class='form-control' name='acabado_galones' id='acabado_galones'></td>";
 
-            echo "<td><input disabled class='form-control' id='acabado_unidades'></td>";
+            echo "<td><input readonly class='form-control' name='acabado_unidades' id='acabado_unidades'></td>";
 
             echo "</tr>";
         }
@@ -149,52 +178,70 @@ var_dump( $_POST );
     
     
     ?>
-</tbody>
-</table>
+  </tbody>
+  </table>
 
-<?php
+  <?php
 
-# Recomendaciones Imprimación ---------------------------------------------------------------------------------------------------------
+  # Recomendaciones Imprimación ---------------------------------------------------------------------------------------------------------
 
-$consulta = "SELECT preparacion_superficie FROM productos WHERE referencia = '$cod_imprimante'";
-$resultado = mysqli_query( $conexion, $consulta ) or die ( "Algo ha ido mal en la consulta a la base de datos");
-$imprimante_preparacion_superficie = mysqli_fetch_array( $resultado );
+  $consulta = "SELECT preparacion_superficie FROM productos WHERE referencia = '$cod_imprimante'";
+  $resultado = mysqli_query( $conexion, $consulta ) or die ( "Algo ha ido mal en la consulta a la base de datos");
+  $imprimante_preparacion_superficie = mysqli_fetch_array( $resultado );
 
-$consulta = "SELECT disolvente FROM productos WHERE referencia = '$cod_imprimante'";
-$resultado = mysqli_query( $conexion, $consulta ) or die ( "Algo ha ido mal en la consulta a la base de datos");
-$imprimante_disolvente = mysqli_fetch_array( $resultado ); 
+  $consulta = "SELECT disolvente FROM productos WHERE referencia = '$cod_imprimante'";
+  $resultado = mysqli_query( $conexion, $consulta ) or die ( "Algo ha ido mal en la consulta a la base de datos");
+  $imprimante_disolvente = mysqli_fetch_array( $resultado ); 
 
-$consulta = "SELECT eps FROM productos WHERE referencia = '$cod_imprimante'";
-$resultado = mysqli_query( $conexion, $consulta ) or die ( "Algo ha ido mal en la consulta a la base de datos");
-$imprimante_espesor_recomendado = mysqli_fetch_array( $resultado );
+  $consulta = "SELECT eps FROM productos WHERE referencia = '$cod_imprimante'";
+  $resultado = mysqli_query( $conexion, $consulta ) or die ( "Algo ha ido mal en la consulta a la base de datos");
+  $imprimante_espesor_recomendado = mysqli_fetch_array( $resultado );
 
-?>
-<br><br><br><br>
-<div class="input-group mb-3">
-  <span class="input-group-text" id="basic-addon1">Preparación de Superficie:</span>
-  <input type="text" class="form-control" aria-describedby="basic-addon1" value="<?php echo $imprimante_preparacion_superficie[0]; ?>">
+  ?>
+  <br><br><br><br>
+  <div class="input-group mb-3">
+    <span class="input-group-text" id="basic-addon1">Preparación de Superficie:</span>
+    <input type="text" name="imprimante_preparacion_superficie" class="form-control" aria-describedby="basic-addon1" value="<?php echo $imprimante_preparacion_superficie[0]; ?>">
+  </div>
+
+  <br>
+  <div class="input-group mb-3">
+    <span class="input-group-text" id="basic-addon1">disolvente:</span>
+    <input type="text" name="imprimante_disolvente" class="form-control" aria-describedby="basic-addon1" value="<?php echo $imprimante_disolvente[0]; ?>">
+  </div>
+  <br>
+  <div class="input-group mb-3">
+    <span class="input-group-text" id="basic-addon1">Espesor Recomendado:</span>
+    <input type="text" name="imprimante_espesor_recomendado" class="form-control" aria-describedby="basic-addon1" value="<?php echo $imprimante_espesor_recomendado[0]; ?> Mils">
+  </div>
+
+
+  <br>
+  <div class="input-group">
+    <span class="input-group-text">Observaciones adicionales:</span>
+    <textarea class="form-control" name="observaciones adicionales" aria-label="With textarea"></textarea>
+  </div>
+  <br><br><br>
+
 </div>
-
-<br>
-<div class="input-group mb-3">
-  <span class="input-group-text" id="basic-addon1">disolvente:</span>
-  <input type="text" class="form-control" aria-describedby="basic-addon1" value="<?php echo $imprimante_disolvente[0]; ?>">
-</div>
-<br>
-<div class="input-group mb-3">
-  <span class="input-group-text" id="basic-addon1">Espesor Recomendado:</span>
-  <input type="text" class="form-control" aria-describedby="basic-addon1" value="<?php echo $imprimante_espesor_recomendado[0]; ?> Mils">
-</div>
-
-
-<br>
-<div class="input-group">
-  <span class="input-group-text">Observaciones adicionales:</span>
-  <textarea class="form-control" aria-label="With textarea"></textarea>
-</div>
-<br><br><br>
-
-</div>
+</form>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
 <script src="tabla.js"></script>
+
+
+
+
+<?php
+/*
+require_once('tabla_general.php'); // llamamos el archivo que se supone contiene el html y dejamoso que se renderize
+$dompdf->load_html(ob_get_clean());//y ponemos todo lo que se capturo con ob_start() para que sea capturado por DOMPDF
+
+$dompdf->set_paper('A4', 'landscape');
+$dompdf->render();
+$dompdf->stream('document.pdf', array("attachment" => true));
+*/
+
+
+include("template/pie.php");
+?>
